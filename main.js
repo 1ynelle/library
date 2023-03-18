085aaee44b533c9918b2952b238d45e162aa019e
Lynelle <l>
2023-03-17 23:58:23 -0700

    Add library to local storage

--- main.js
+++ main.js
@@ -9,121 +9,27 @@ const library = document.querySelector('.library > .container');
 const statusCheckbox = '.status input[type="checkbox"]';
 const removeBtn = '.book-wrapper .remove-btn';
 
-let myLibrary = [];
-
-// Modal
-// Disable background element keyboard interactivity
-const closeModal = () => modalWindow.classList.toggle('open');
+// MODAL FUNCTIONS //////////
 const openModal = () => modalWindow.classList.toggle('open');
+const closeModal = () => modalWindow.classList.toggle('open');
 
-// Book
-function createRemoveBtn() {
-  const removeBtn = document.createElement('button');
-  removeBtn.className = 'remove-btn icon';
-  removeBtn.type = 'button';
-  removeBtn.ariaLabel = 'remove';
-
-  return removeBtn;
-}
-
-// Button on each book to change read status (on Book prototype)
-function changeStatus(e) {
-  const checkbox = e.target;
-  const bookWrapper = checkbox.closest('.book-wrapper');
-  const index = bookWrapper.dataset.index;
-  const label = checkbox.nextElementSibling;
-
-  if (checkbox.checked) {
-    myLibrary[index].status = 'Finished';
-  } else {
-    myLibrary[index].status = 'Reading';
-  }
-
-  label.innerText = myLibrary[index].status;
-}
-
-function createStatus(book, index) {
-  // Div Wrapper
-  const status = document.createElement('div');
-  status.className = 'status';
-
-  // Input
-  const checkbox = document.createElement('input');
-  checkbox.className = 'icon';
-  checkbox.type = 'checkbox';
-  checkbox.name = 'status';
-  checkbox.id = `book${index}-status`;
-
-  if (book.status === 'Finished') {
-    checkbox.checked = true;
-  }
-
-  // Label
-  const label = document.createElement('label');
-  label.setAttribute('for', `book${index}-status`);
-  label.append(document.createTextNode(book.status));
-
-  // Append Elements
-  status.append(checkbox, label);
-
-  return status;
-}
-
-function createBookElement(book, index) {
-  const newBook = document.createElement('div');
-  newBook.className = 'book';
-
-  // Title and Author Wrapper
-  const bookInfo = document.createElement('div');
-  bookInfo.className = 'book__info';
-
-  const title = document.createElement('h2');
-  title.className = 'book__title';
-  title.append(document.createTextNode(book.title));
-
-  const author = document.createElement('p');
-  author.className = 'book__author';
-  author.append(document.createTextNode(book.author));
-
-  // Read Status
-  const status = createStatus(book, index);
-
-  // Append Elements
-  bookInfo.append(title, author);
-  newBook.append(bookInfo, status);
-
-  return newBook;
-}
-
-function resetLibrary() {
-  const bookWrappers = library.querySelectorAll('.book-wrapper');
-  bookWrappers.forEach((wrapper) => wrapper.remove());
-}
-
-// Loop through array and display on page
-function displayBooks() {
-  resetLibrary();
-
-  myLibrary.forEach((book, index) => {
-    const bookWrapper = document.createElement('article');
-    bookWrapper.className = 'book-wrapper';
-    bookWrapper.dataset.index = `${index}`;
-
-    const newBook = createBookElement(book, index);
-    const removeBtn = createRemoveBtn();
-
-    bookWrapper.append(newBook, removeBtn);
-    library.appendChild(bookWrapper);
+// BOOK FUNCTIONS //////////
+function addGlobalEventListener(type, selector, callback) {
+  document.addEventListener(type, (e) => {
+    if (e.target.matches(selector)) callback(e);
   });
 }
 
-// Button on each book to remove book from library (data attribute corresponding to index)
-function removeBook(e) {
-  const bookWrapper = e.target.parentElement;
-  const index = bookWrapper.dataset.index;
+// Add a book to the library ----------
+function handleBookSubmit(e) {
+  e.preventDefault();
+  closeModal();
 
-  myLibrary.splice(index, 1);
-  displayBooks();
+  const newBookObj = createBookObj();
+  addBookToStorage(newBookObj);
+  addBookToDOM(newBookObj);
+
+  bookForm.reset();
 }
 
 function Book(title, author, status) {
@@ -133,38 +39,184 @@ function Book(title, author, status) {
 }
 /* Book.prototype.changeStatus = (status) => (this.status = status); */
 
-// Take user input, store new books into array
-function addBookToLibrary(e) {
-  const title = bookForm.querySelector('input[name="book-title"]').value;
-  const author = bookForm.querySelector('input[name="book-author"]').value;
-  const status = bookForm.querySelector('select[name="book-status"]').value;
+function createBookObj() {
+  const bookFormData = new FormData(bookForm);
+  const bookTitle = bookFormData.get('book-title');
+  const bookAuthor = bookFormData.get('book-author');
+  const bookStatus = bookFormData.get('book-status');
 
-  const book = new Book(title, author, status);
-  myLibrary.push(book);
+  const newBookObj = new Book(bookTitle, bookAuthor, bookStatus);
+
+  return newBookObj;
 }
 
-function addGlobalEventListener(type, selector, callback) {
-  document.addEventListener(type, (e) => {
-    if (e.target.matches(selector)) callback(e);
+function getLibraryFromStorage() {
+  let myLibraryArr;
+
+  if (localStorage.getItem('myLibrary') === null) {
+    myLibraryArr = [];
+  } else {
+    myLibraryArr = JSON.parse(localStorage.getItem('myLibrary'));
+  }
+
+  return myLibraryArr;
+}
+
+function addBookToStorage(bookObj) {
+  const myLibraryArr = getLibraryFromStorage();
+  myLibraryArr.push(bookObj);
+  localStorage.setItem('myLibrary', JSON.stringify(myLibraryArr));
+}
+
+function addBookToDOM(bookObj) {
+  // Book Wrapper
+  const bookWrapper = document.createElement('div');
+  bookWrapper.className = 'book-wrapper';
+
+  // Book
+  const bookElem = document.createElement('div');
+  bookElem.className = 'book';
+  const bookInfo = createBookInfo(bookObj);
+  const status = createStatus(bookObj);
+  bookElem.append(bookInfo, status);
+
+  // Remove Button
+  const removeBtn = createRemoveBtn();
+
+  // Append Elements
+  bookWrapper.append(bookElem, removeBtn);
+  library.append(bookWrapper);
+}
+
+// Title and Author Wrapper
+function createBookInfo(bookObj) {
+  const bookInfo = document.createElement('div');
+  bookInfo.className = 'book__info';
+
+  const title = document.createElement('h2');
+  title.className = 'book__title';
+  title.append(document.createTextNode(bookObj.title));
+
+  const author = document.createElement('p');
+  author.className = 'book__author';
+  author.append(document.createTextNode(bookObj.author));
+
+  bookInfo.append(title, author);
+
+  return bookInfo;
+}
+
+// Read Status Element
+function createStatus(bookObj) {
+  const bookTitle = bookObj.title.split(' ').join('-').toLowerCase();
+
+  // Div Wrapper
+  const status = document.createElement('div');
+  status.className = 'status';
+
+  // Input
+  const checkbox = document.createElement('input');
+  checkbox.className = 'icon';
+  checkbox.type = 'checkbox';
+  checkbox.name = 'status';
+  checkbox.id = `${bookTitle}-status`;
+
+  if (bookObj.status === 'Finished') {
+    checkbox.checked = true;
+  }
+
+  // Label
+  const label = document.createElement('label');
+  label.setAttribute('for', `${bookTitle}-status`);
+  label.append(document.createTextNode(bookObj.status));
+
+  // Append Elements
+  status.append(checkbox, label);
+
+  return status;
+}
+
+function createRemoveBtn() {
+  const removeBtn = document.createElement('button');
+  removeBtn.className = 'remove-btn icon';
+  removeBtn.type = 'button';
+  removeBtn.ariaLabel = 'remove';
+
+  return removeBtn;
+}
+
+// Removing a book --------------------
+function handleBookRemove(e) {
+  // Get book identifier
+  const bookElem = e.target.previousElementSibling;
+  const title = bookElem.querySelector('.book__title').innerText;
+  const author = bookElem.querySelector('.book__author').innerText;
+
+  if (!confirm(`Remove ${title}?`)) return;
+
+  removeBookFromStorage(title, author);
+  // Remove book from DOM
+  bookElem.parentElement.remove();
+}
+
+function removeBookFromStorage(title, author) {
+  let myLibraryArr = getLibraryFromStorage();
+
+  myLibraryArr = myLibraryArr.filter((bookObj) => {
+    return !(bookObj.title === title && bookObj.author === author);
   });
+
+  localStorage.setItem('myLibrary', JSON.stringify(myLibraryArr));
 }
 
-// Modal
-addBtn.addEventListener('click', openModal);
-modalWindow.addEventListener('click', closeModal);
-modal.addEventListener('click', (e) => e.stopPropagation());
-closeBtn.addEventListener('click', closeModal);
+// Changing a book's read status --------------------
+function changeStatus(e) {
+  const checkbox = e.target;
+  const label = checkbox.nextElementSibling;
+  const bookInfo = checkbox.parentElement.previousElementSibling;
+  const title = bookInfo.querySelector('.book__title').innerText;
+  const author = bookInfo.querySelector('.book__author').innerText;
 
-// 'Add a new book button' that brings up a form allowing users to input details for new book (preventDefault)
-bookForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+  let status;
+  if (checkbox.checked) {
+    status = 'Finished';
+  } else {
+    status = 'Reading';
+  }
 
-  addBookToLibrary(e);
-  bookForm.reset();
-  closeModal();
-  displayBooks();
-});
+  changeStorageBookStatus(title, author, status);
+  // Change DOM book status
+  label.innerText = status;
+}
 
-// Book
-addGlobalEventListener('click', statusCheckbox, changeStatus);
-addGlobalEventListener('click', removeBtn, removeBook);
+function changeStorageBookStatus(title, author, status) {
+  const myLibraryArr = getLibraryFromStorage();
+
+  myLibraryArr.forEach((bookObj) => {
+    if (bookObj.title === title && bookObj.author === author) {
+      bookObj.status = status;
+    }
+  });
+
+  localStorage.setItem('myLibrary', JSON.stringify(myLibraryArr));
+}
+
+// EVENT LISTENERS //////////
+function initApp() {
+  // Show library in DOM
+  const myLibraryArr = getLibraryFromStorage();
+  myLibraryArr.forEach((bookObj) => addBookToDOM(bookObj));
+
+  // Modal
+  addBtn.addEventListener('click', openModal);
+  modalWindow.addEventListener('click', closeModal);
+  modal.addEventListener('click', (e) => e.stopPropagation());
+  closeBtn.addEventListener('click', closeModal);
+
+  // Book
+  bookForm.addEventListener('submit', handleBookSubmit);
+  addGlobalEventListener('click', removeBtn, handleBookRemove);
+  addGlobalEventListener('click', statusCheckbox, changeStatus);
+}
+
+initApp();
